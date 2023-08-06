eb37863a14b9b4e6a820d25a3c4ea8bbef10a68e
raccoonjeong <rana3782@gmail.com>
2023-08-06 15:33:49 +0900

    Initial commit

---
+++ src/test/java/org/raccoon/dao/ConnectTests.java
@@ -0,0 +1,15 @@
+package org.raccoon.dao;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ConnectTests {
+
+    @Test
+    public void test1() {
+        int v1 = 10;
+        int v2 = 10;
+
+        Assertions.assertEquals(v1, v2);
+    }
+}
